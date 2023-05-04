bbfdbcfd1a674a828c0c2045aa84e08d73c1dd44
USER <varelantoni@gmail.com>
2023-05-04 11:06:29 +0700

    CV.Labkomdas

---
+++ src/PraktikumPemrogramanLanjut4/ClassJaket.java
@@ -0,0 +1,67 @@
+package PraktikumPemrogramanLanjut4;
+
+class Jaket{
+    String Tipe;
+    int Harga;
+    int Jumlah;
+    final int JaketA = 100000;
+    final int JaketB = 125000;
+    final int JaketC = 175000;
+
+    public Jaket(String Tipe, int Jumlah){
+        this.Tipe = Tipe;
+        this.Jumlah = Jumlah;
+    }
+
+    void HargaJaketA(){
+        if (Jumlah>100)
+            Harga = JaketA - 5000;
+        else
+            Harga = JaketA;
+    }
+
+    void HargaJaketB(){
+        if (Jumlah>100)
+            Harga = JaketB - 5000;
+        else
+            Harga = JaketB;
+    }
+
+    void HargaJaketC(){
+        if (Jumlah>100)
+            Harga = JaketC - 15000;
+        else
+            Harga = JaketC;
+    }
+
+    void display(){
+        if (Tipe.equalsIgnoreCase("1")) {
+            HargaJaketA();
+            setPesanan();
+        }
+        else if (Tipe.equalsIgnoreCase("2")) {
+            HargaJaketB();
+            setPesanan();
+        }
+        else if (Tipe.equalsIgnoreCase("3")) {
+            HargaJaketC();
+            setPesanan();
+        }
+        else System.out.println("[ERROR]");
+    }
+
+    void setPesanan(){
+        String jenis_jaket = null;
+        if (Tipe.equalsIgnoreCase("1"))
+            jenis_jaket = "Jaket A";
+        else if (Tipe.equalsIgnoreCase("2")) {
+            jenis_jaket = "Jaket B";
+        } else if (Tipe.equalsIgnoreCase("3")) {
+            jenis_jaket = "Jaket C";
+        }
+        System.out.println("Jenis yang anda beli : " + jenis_jaket);
+        System.out.println("Harga                : " + Harga);
+        System.out.println("Jumlah jaket         : " + Jumlah + " pcs");
+        System.out.println("Total harga          : " + Harga*Jumlah);
+    }
+}
---
+++ src/PraktikumPemrogramanLanjut4/main.java
@@ -0,0 +1,27 @@
+package PraktikumPemrogramanLanjut4;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("======= Toko Labkomdas =======");
+        System.out.println("Menyediakan 3 Tipe Jaket");
+        System.out.println("1. Jaket A : Rp. 100.000");
+        System.out.println("2. Jaket B : Rp. 125.000");
+        System.out.println("3. Jaket C : Rp. 175.000");
+        System.out.println("Pilih Jenis Jaket");
+        System.out.print("Tipe jaket yang anda pesan: ");
+        String tipe = input.next();
+        System.out.print("Jumlah jaket yang anda pesan: ");
+        int jumlah = input.nextInt();
+        System.out.println("");
+        Jaket pesanan = new Jaket(tipe, jumlah);
+        pesanan.display();
+
+
+        
+    }
+}
